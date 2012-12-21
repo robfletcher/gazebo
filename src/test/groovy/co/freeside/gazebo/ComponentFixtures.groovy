b7c66c0ba96f78afd2f5c16d8f46163573c46154
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-12-21 00:02:26 +0000

    moved fixture projects so I can use a single classpath in IntelliJ

--- src/test/groovy/co/freeside/gazebo/ComponentFixtures.groovy
+++ src/test/groovy/co/freeside/gazebo/ComponentFixtures.groovy
@@ -2,9 +2,9 @@ package co.freeside.gazebo
 
 class ComponentFixtures {
 
-	def standardProjectHome = resourceDirectory('/standard')
-	def nonStandardProjectHome = resourceDirectory('/nonStandard')
-	def customDirProjectHome = resourceDirectory('/customDir')
+	def standardProjectHome = resourceDirectory('/projects/standard')
+	def nonStandardProjectHome = resourceDirectory('/projects/nonStandard')
+	def customDirProjectHome = resourceDirectory('/projects/customDir')
 	def componentDir = new File(standardProjectHome, 'components')
 
 	File resourceDirectory(String name) {
